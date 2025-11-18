79d9fcc89c3f072fca9f803441578276f81fa6f2
Evan-Baumgartner <evan.baum@ku.edu>
2025-11-18 12:36:41 -0600

    Test for prompt 1

---
+++ assignment7.cpp
@@ -0,0 +1,32 @@
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/statement.h>
+#include <cppconn/resultset.h>
+#include <cppconn/exception.h>
+
+sql::mysql::MySQL_Driver *driver;
+sql::Connection *con; // to establish connection
+sql::Statement *stmt; // to execute SQL statement
+sql::ResultSet *res;  // a place to store the query results
+
+driver = sql::mysql::get_mysql_driver_instance();
+con = driver->connect("mysql.eecs.ku.edu", "348f25_e724b371", "Jah7EWek");
+
+stmt = con->createStatement();
+
+stmt->execute("USE 348f25_e724b371"); // entering database
+
+cout << 'Prompt 1:' << endl;
+res = stmt->executeQuery("SELECT * FROM Student WHERE StdMajor LIKE 'IS'");
+while (res->next())
+{
+    // You can use either numeric offsets...
+    // getInt(1) returns the first column
+    cout << "id = " << res->getInt(1);
+    // ... or column names for accessing results (recommended).
+    cout << ", label = '" << res->getString("label") << "'" << endl;
+}
+
+delete stmt;
+delete con;
+delete con;
